06227d9eb8a888457806b3b97ce84f541d1892d2
Thomas Lombardot <thomas.lombardot@gmail.com>
2026-01-30 21:36:02 +0100

    Feat : Add Player Movement & Collision

---
+++ scripts/player.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+@export var speed = 150
+
+func _physics_process(delta):
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	if direction:
+		velocity = direction * speed
+	else:
+		velocity = Vector2.ZERO
+
+	move_and_slide()
